e47eb4216b113a7f6063914eeb82d486e246793b
김창윤 <kbsc9090@gmail.com>
2026-01-05 17:04:26 +0900

    update

--- src/components/Policy333.jsx
+++ src/components/Policy333.jsx
@@ -3,13 +3,13 @@ export default function Policy333() {
     <section className="card">
       <div className="imgGrid">
         <figure className="imgCard">
-          <img src="/images/il.png" alt="포스코 3대 산업기반" />
+          <img src="/images/il1.png" alt="포스코 3대 산업기반" />
         </figure>
         <figure className="imgCard">
-          <img src="/images/ei.png" alt="미래 3대 공단확장 전략" />
+          <img src="/images/ei1.png" alt="미래 3대 공단확장 전략" />
         </figure>
         <figure className="imgCard">
-          <img src="/images/sam.png" alt="도심 관광 상권 활성화" />
+          <img src="/images/sam1.png" alt="도심 관광 상권 활성화" />
         </figure>
       </div>
     </section>
