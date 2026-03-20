0e558e572ada654a7e4cc6139b5c8ebcee4f6bb2
kimcy2 <kbsc9090@gmail.com>
2026-03-20 13:29:11 +0900

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="wrap footer card">
-      <p>본 사이트에 사용된 일부 이미지와 동영상은 AI기술을 활용해 제작되었습니다.</p>
-      <p>© 공식 페이지 | 포항을 위해 공원식이 달려갑니다.</p>
-    </footer>
-  );
-}
